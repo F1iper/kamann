042d9180f687463e55d553f1d7d3234667e6454f
f1iper <ftimofiejew@gmail.com>
2024-11-14 11:20:28 +0100

    Initial project version

---
+++ src/test/java/pl/kamann/KamannApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
