edd28c31b1b0b9629a3028cf15845797013ed666
f1iper <ftimofiejew@gmail.com>
2024-11-25 22:38:19 +0100

    Add dev profile to omit security config

--- src/main/java/pl/kamann/config/security/SecurityConfig.java
+++ src/main/java/pl/kamann/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -52,6 +53,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Profile(value = "prod")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -74,6 +76,13 @@ public class SecurityConfig {
                 )
                 .build();
     }
+
+    @Bean
+    @Profile(value = "dev")
+    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
