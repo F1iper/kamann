482da5af347660b757fe5ffeee9a07907c373075
f1iper <ftimofiejew@gmail.com>
2024-12-06 23:26:04 +0100

    Clean-up imports

--- src/main/java/pl/kamann/repositories/admin/AttendanceStatRepository.java
+++ src/main/java/pl/kamann/repositories/admin/AttendanceStatRepository.java
@@ -1,9 +1,7 @@
 package pl.kamann.repositories.admin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import pl.kamann.entities.reports.AttendanceStatEntity;
 
-@Repository
 public interface AttendanceStatRepository extends JpaRepository<AttendanceStatEntity, Long> {
 }
--- src/main/java/pl/kamann/repositories/admin/RevenueStatRepository.java
+++ src/main/java/pl/kamann/repositories/admin/RevenueStatRepository.java
@@ -1,9 +1,7 @@
 package pl.kamann.repositories.admin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import pl.kamann.entities.reports.RevenueStatEntity;
 
-@Repository
 public interface RevenueStatRepository extends JpaRepository<RevenueStatEntity, Long> {
 }
