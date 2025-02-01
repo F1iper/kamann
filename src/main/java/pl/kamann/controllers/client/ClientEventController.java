133cd406bc045450d954942b88d8330478ff3bcf
f1iper <ftimofiejew@gmail.com>
2025-02-01 12:39:40 +0100

    Add Tags for swagger, to simplify development

--- src/main/java/pl/kamann/controllers/client/ClientEventController.java
+++ src/main/java/pl/kamann/controllers/client/ClientEventController.java
@@ -1,6 +1,7 @@
 package pl.kamann.controllers.client;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/client/events")
 @RequiredArgsConstructor
+@Tag(name = "/api/client/events", description = "Client events controller")
 public class ClientEventController {
 
     private final ClientEventService clientEventService;
