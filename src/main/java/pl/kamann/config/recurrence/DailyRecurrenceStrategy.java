c138c136ef1fab12cd2046407b4db721e4b8cc0e
F1iper <ftimofiejew@gmail.com>
2025-02-05 16:27:28 +0100

    Add strategies to generate Event occurrencies

---
+++ src/main/java/pl/kamann/config/recurrence/DailyRecurrenceStrategy.java
@@ -0,0 +1,22 @@
+package pl.kamann.config.recurrence;
+
+import pl.kamann.entities.event.Event;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DailyRecurrenceStrategy implements RecurrenceStrategy {
+
+    @Override
+    public List<LocalDateTime> generateOccurrences(Event event, LocalDateTime until) {
+        List<LocalDateTime> occurrences = new ArrayList<>();
+        LocalDateTime current = event.getStart();
+
+        while (!current.isAfter(until)) {
+            occurrences.add(current);
+            current = current.plusDays(1);
+        }
+        return occurrences;
+    }
+}
