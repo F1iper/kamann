2a2cbe3fb7bed7a3c3bbd8c3a1b187e51c3bc432
F1iper <ftimofiejew@gmail.com>
2025-02-24 22:07:54 +0100

    Avoid installing docker-compose in workflow environment

--- src/test/java/pl/kamann/testcontainers/config/TestContainersConfig.java
+++ src/test/java/pl/kamann/testcontainers/config/TestContainersConfig.java
@@ -1,7 +1,7 @@
 package pl.kamann.testcontainers.config;
 
-import jakarta.annotation.PreDestroy;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -9,23 +9,21 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration
 public class TestContainersConfig {
 
-    private static final PostgreSQLContainer<?> POSTGRES_CONTAINER;
+    private static final PostgreSQLContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres:16"))
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
 
     static {
-        POSTGRES_CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
-                .withDatabaseName("testdb")
-                .withUsername("test")
-                .withPassword("test");
         POSTGRES_CONTAINER.start();
-
-        // Set Spring properties dynamically
         System.setProperty("spring.datasource.url", POSTGRES_CONTAINER.getJdbcUrl());
         System.setProperty("spring.datasource.username", POSTGRES_CONTAINER.getUsername());
         System.setProperty("spring.datasource.password", POSTGRES_CONTAINER.getPassword());
     }
 
-    @PreDestroy
-    public void stopContainer() {
-        POSTGRES_CONTAINER.stop();
+    @Bean
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return POSTGRES_CONTAINER;
     }
 }
